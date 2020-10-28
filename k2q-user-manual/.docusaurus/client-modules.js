c0d1ff33fbad257bf32144b6488676dc1211022b
lukehigs <63781400+lukehigs@users.noreply.github.com>
2020-10-28 20:11:11 +0000

    restore to original

---
+++ k2q-user-manual/.docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("C:\\Users\\higgi\\Documents\\GitHub\\keytoqualitysite\\k2q-user-manual\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\higgi\\Documents\\GitHub\\keytoqualitysite\\k2q-user-manual\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\higgi\\Documents\\GitHub\\keytoqualitysite\\k2q-user-manual\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\higgi\\Documents\\GitHub\\keytoqualitysite\\k2q-user-manual\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\higgi\\Documents\\GitHub\\keytoqualitysite\\k2q-user-manual\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
+  require("C:\\Users\\higgi\\Documents\\GitHub\\keytoqualitysite\\k2q-user-manual\\src\\css\\custom.css"),
+];
