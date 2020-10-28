10d675dfdf67bac2289edefb3b53fafeadf94614
lukehigs <63781400+lukehigs@users.noreply.github.com>
2020-10-28 20:22:38 +0000

    restore to original

--- 3faa0d8d.e53290dc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{64:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/k2q-user-manual/blog/welcome"},{"title":"Hello","permalink":"/k2q-user-manual/blog/hello-world"},{"title":"Hola","permalink":"/k2q-user-manual/blog/hola"}]}')}}]);
--- k2q-user-manual/build/668d785b.9d6ccebf.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{67:function(a){a.exports=JSON.parse('{"allTagsPath":"/k2q-user-manual/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/k2q-user-manual/blog/tags/hola"}')}}]);
